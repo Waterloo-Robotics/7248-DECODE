0c0ebcb0a1a39d9ad1b566b979b1c1fe61345260
Waterloo Robotics <ftcteam7247@gmail.com>
2025-10-05 16:49:29 -0500

    Update shooterTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooterTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/shooterTeleop.java
@@ -1,48 +1,63 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp(name = "shooterTeleop", group = "Concept")
 public class shooterTeleop extends OpMode {
-
-    // declare motors. "motor" is a placeholder. rename to flywheel, or other desired motor names
-    // in the future. be sure to also modify the hardware map on ds - kyeler
     private DcMotor motor; // flywheel motor
+    private int motor_speed = 0; // 0 = off, 1-4 = power levels
+    private boolean prevRightBumper = false;
+    private boolean prevLeftBumper = false;
 
-   // this code runs in initialize, typically used to set motor power and grab motors, servos, and
-    // any other hardware from the hardware map
-@Override
+    @Override
     public void init() {
-    motor = hardwareMap.get(DcMotor.class, "motor");
-}
+        motor = hardwareMap.get(DcMotor.class, "motor");
+    }
 
-@Override
-    public void start() {
-
-}
-
-@Override
+    @Override
     public void loop() {
+        boolean rightBumperPressed = gamepad1.right_bumper;
+        boolean leftBumperPressed = gamepad1.left_bumper;
 
-    // 1/4 total power
-    if (gamepad1.dpad_left) {
-        motor.setPower(0.25);
+        // Right bumper: increase speed
+        if (rightBumperPressed && !prevRightBumper) {
+            if (motor_speed < 4) {
+                motor_speed++;
+            }
+        }
+
+        if (leftBumperPressed && !prevLeftBumper) {
+            motor_speed = 0;
+        }
+
+        double power = 0.0;
+        switch (motor_speed) {
+            case 1:
+                power = 0.25;
+                break;
+            case 2:
+                power = 0.5;
+                break;
+            case 3:
+                power = 0.75;
+                break;
+            case 4:
+                power = 1.0;
+                break;
+            default:
+                power = 0.0;
+                break;
+        }
+        motor.setPower(power);
+
+        // Telemetry feedback
+        telemetry.addData("Motor Speed Level", motor_speed);
+        telemetry.update();
+
+        // Store previous button states
+        prevRightBumper = rightBumperPressed;
+        prevLeftBumper = leftBumperPressed;
     }
-    // half total power
-    if (gamepad1.dpad_up) {
-        motor.setPower(0.5);
-    }
-    // 3/4 total power
-    if (gamepad1.dpad_right) {
-        motor.setPower(0.75);
-    }
-    // full power
-    if (gamepad1.dpad_right) {
-        motor.setPower(1);
-    }
-    // deactivate motor
-    if (gamepad1.rightBumperWasPressed()) {
-        motor.setPower(0);
-    }
-}
 }
