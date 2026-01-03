06c6d1b58756778c71a52935587761ddcb3455d6
Waterloo Robotics <ftcteam7247@gmail.com>
2026-01-03 07:50:16 -0600

    Update auto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto.java
@@ -10,45 +10,43 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous(name="Leave Auto")
 public class auto extends LinearOpMode {
-    public DcMotor frontLeftMotor = null;
-    public DcMotor frontRightMotor = null;
-    public DcMotor backLeftMotor = null;
-    public DcMotor backRightMotor = null;
+    public DcMotor left_drive = null;
+    public DcMotor right_drive = null;
+    public DcMotor left_2drive = null;
+    public DcMotor right_2drive = null;
     public GoBildaPinpointDriver ppointdr= null;
 
     private ElapsedTime runtime = new ElapsedTime();
 
     // we use a varible so we can change the power easily later on
-    static final double FORWARD_POWER = 0.5;
+    static final double FORWARD_POWER = 0.1;
     static final long DRIVE_TIME_MS = 2800;
+
 
 
 
     @Override
     public void runOpMode() {
-        frontLeftMotor = hardwareMap.get(DcMotor.class, "FrontLeftMotor");
-        frontRightMotor = hardwareMap.get(DcMotor.class, "FrontLeftMotor");
-        backLeftMotor = hardwareMap.get(DcMotor.class, "FrontLeftMotor");
-        backRightMotor = hardwareMap.get(DcMotor.class, "FrontLeftMotor");
-        ppointdr = hardwareMap.get(GoBildaPinpointDriver.class, "ppdriver");
+        left_drive = hardwareMap.get(DcMotor.class, "left_drive");
+        right_drive = hardwareMap.get(DcMotor.class, "right_drive");
+        left_2drive = hardwareMap.get(DcMotor.class, "left_2drive");
+        right_2drive = hardwareMap.get(DcMotor.class, "right_2drive");
+        left_drive.setDirection(DcMotor.Direction.FORWARD);
+        right_drive.setDirection(DcMotor.Direction.REVERSE);
+        left_2drive.setDirection(DcMotor.Direction.FORWARD);
+        right_2drive.setDirection(DcMotor.Direction.REVERSE);
 
-        ppointdr.setOffsets(-175, 115, DistanceUnit.MM);
-        ppointdr.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
 
-
-        frontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        frontRightMotor.setDirection(DcMotor.Direction.FORWARD);
-        backLeftMotor.setDirection(DcMotor.Direction.FORWARD);
-        backRightMotor.setDirection(DcMotor.Direction.REVERSE);
-
+        waitForStart();
         if (opModeIsActive()) {
+
             // SLEEP = Time to run command before moving to the next, in miliseconds
             sleep(750);
-            frontLeftMotor.setPower(FORWARD_POWER);
-            frontRightMotor.setPower(FORWARD_POWER);
-            backLeftMotor.setPower(FORWARD_POWER);
-            backRightMotor.setPower(FORWARD_POWER);
-            sleep(900);
+            left_drive.setPower(-0.5);
+            right_drive.setPower(-0.5);
+            left_2drive.setPower(-0.5);
+            right_2drive.setPower(-0.5);
+            sleep(800);
         }
 
     }}
